ba20cca693f93b82b79cf6a0028047f24de87fe8
Nawfel Ajari <ajari.nawfel@hotmail.com>
2024-02-09 22:12:40 +0100

    chore: front of homepage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,44 +37,39 @@
         <div x-data="{ open: false }">
             <nav class="flex items-center justify-between px-4 py-8">
                 <button @click="open = !open" class="space-y-2 focus:outline-none">
-                    <!-- Icône du menu (hamburger) -->
+                    <!-- Icône du menu (hamburger) pour mobile -->
                     <div class="w-8 h-0.5 bg-white"></div>
                     <div class="w-8 h-0.5 bg-white"></div>
                     <div class="w-8 h-0.5 bg-white"></div>
                 </button>
                 <!-- Logo -->
                 <a href="/" class="text-3xl text-white uppercase logo">bxcars</a>
-                <div>
+                <div class="hidden sm:flex">
                     @if(Route::has('login'))
                     @auth
                     <a class="text-white pr-2 hover:text-yellow-500" href="{{ url('/dashboard') }}"
-                        style="cursor: pointer;">
-                        ADMIN
-                    </a>
-                    <span class="text-white pr-2">
-                        |
-                    </span>
+                        style="cursor: pointer;">ADMIN</a>
+                    <span class="text-white pr-2">|</span>
                     <span class="pr-4 text-white hover:text-yellow-500" style="cursor: pointer;"
                         onclick="window.location.href='{{ url('profile') }}'">{{ Auth::user()->name }}</span>
                     <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         <img class="inline cursor-pointer h-7" src="{{ asset('logout.png') }}" alt="Déconnexion">
                     </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf
                     </form>
                     @endauth
                     @endif
                 </div>
             </nav>
 
-
-            <div class="absolute top-0 left-0 w-40 h-screen transition-transform duration-200 transform bg-black"
+            <!-- Sidebar mobile -->
+            <div class="absolute top-0 left-0 w-64 h-screen transition-transform duration-200 transform bg-black"
                 :class="{'-translate-x-full': !open, 'translate-x-0': open}">
                 <button @click="open = false" class="p-4 text-white hover:text-red-400">
-                    <img src="/close-menu-icon.png" alt="closing the menu button icon"
-                        class="w-1/2 rounded-full h-1/2 ">
+                    <img src="/close-menu-icon.png" alt="closing the menu button icon" class="w-8 h-8">
                 </button>
                 <div class="flex flex-col p-4">
+                    <!-- Liens de navigation -->
                     <a href="{{ url('/') }}" class="py-2 text-yellow-500 border-b-2 border-gray-100">Accueil</a>
                     <a href="{{ url('/services') }}" class="py-2 text-white hover:text-yellow-500">Services</a>
                     <a href="{{ url('/about') }}" class="py-2 text-white hover:text-yellow-500">À propos</a>
@@ -85,13 +80,18 @@
                     <a href="{{ url('/membres') }}" class="py-2 text-gray-400 hover:text-yellow-500">Membres</a>
                     <a href="{{ url('/cars/create') }}" class="py-2 text-gray-400 hover:text-yellow-500">MyCARS</a>
                     <a href="{{ url('/user/create') }}" class="py-2 text-gray-400 hover:text-yellow-500">MyADMIN</a>
+                    <a href="#" class="sm:hidden py-2 text-gray-400 hover:text-yellow-500"
+                        onclick="event.preventDefault(); document.getElementById('logout-form-mobile').submit();">
+                        <img class="inline cursor-pointer h-3" src="{{ asset('logout.png') }}" alt="Déconnexion">
+                        Déconnexion
+                    </a>
                     @endauth
                     @endif
                 </div>
             </div>
-
         </div>
-        <h1 class="mt-20 text-5xl font-bold text-center text-white lg:px-32">
+
+        <h1 class="mt-20 text-3xl font-bold text-center text-white lg:text-5xl lg:px-32">
             Votre clé pour explorer le Maroc, confort et liberté garantis
         </h1>
     </div>
@@ -118,7 +118,7 @@
         @endif
 
 
-        <div class="grid grid-cols-3 grid-rows-2 gap-4 mt-20 lg:px-20">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-4 mt-20 lg:px-20">
             @foreach ($cars as $car)
             <div class="flex flex-col p-2 transition-all bg-white border-2 hover:border-black border-gray rounded-3xl">
                 <img src="{{ Storage::url($car->photo) }}" alt="Car Image">
@@ -188,31 +188,28 @@
             </div>
             @endforeach
         </div>
-
-
         </div>
-
         </div>
         <div class="flex justify-center lg:py-12">
             <button id="voirPlusBtn"
-                class="text-white transition-colors bg-black border-2 rounded-3xl hover:bg-gray-500 border-inherit hover:border-black">
-                <h4 class="w-full px-8 py-4">Voir plus</h4>
+                class="transition-colors bg-black border-2 border-black text-white hover:bg-gray-500 hover:border-black rounded-3xl mt-5">
+                <h4 class="px-4 py-2 lg:px-8 lg:py-4 text-sm lg:text-base">Voir plus</h4>
             </button>
         </div>
     </section>
-
-    <section class="px-32 py-32">
-        <h1 class="text-5xl font-semibold text-center">Comment ça marche</h1>
-        <h3 class="mt-4 text-lg text-center lg:px-56">
+    <section class="px-4 py-8 sm:px-8 md:px-16 lg:px-32 lg:py-32">
+        <h1 class="text-3xl font-semibold text-center md:text-4xl lg:text-5xl">Comment ça marche</h1>
+        <h3 class="mt-4 text-center text-base md:text-lg lg:px-56">
             Louer une voiture au Maroc n'a jamais été aussi simple. Notre processus optimisé rend la réservation et la
             confirmation de votre véhicule de choix en ligne facile et rapide.
         </h3>
 
-        <div class="flex flex-row mt-16">
-            <div class="relative flex flex-col gap-4">
-                <div class="flex flex-row items-center gap-4 p-8 mt-16 bg-white border-2 border-gray rounded-3xl ">
+        <div class="flex flex-col lg:flex-row mt-16">
+            <div class="relative flex flex-col gap-4 lg:flex-1">
+                <!-- Bloc 1 -->
+                <div class="flex flex-col lg:flex-row items-center gap-4 p-8 bg-white border-2 border-gray rounded-3xl">
                     <div class="flex items-center h-full px-4 bg-gray-200 rounded-xl">
-                        <img src="/search.png" alt="mangnifier icon for search" class="icons-2">
+                        <img src="/search.png" alt="magnifier icon for search" class="icons-2">
                     </div>
                     <div class="flex flex-col w-full gap-4">
                         <h3 class="text-xl font-semibold">Parcourez et sélectionnez</h3>
@@ -220,37 +217,41 @@
                             charge qui vous conviennent le mieux.</p>
                     </div>
                 </div>
-                <div class="flex flex-row items-center gap-4 p-8 bg-white border-2 border-gray rounded-3xl ">
+                <!-- Bloc 2 -->
+                <div class="flex flex-col lg:flex-row items-center gap-4 p-8 bg-white border-2 border-gray rounded-3xl">
                     <div class="flex items-center h-full px-4 bg-gray-200 rounded-xl">
-                        <img src="/calendar.png" alt="calendar icon" class="icons-2 ">
+                        <img src="/calendar.png" alt="calendar icon" class="icons-2">
                     </div>
-                    <div class="flex flex-col gap-4">
-                        <h3 class="text-xl font-semibold">
-                            Réservez et confirmez</h3>
+                    <div class="flex flex-col w-full gap-4">
+                        <h3 class="text-xl font-semibold">Réservez et confirmez</h3>
                         <p>Réservez la voiture de votre choix en seulement quelques clics et recevez une confirmation
                             instantanée par e-mail ou SMS.</p>
                     </div>
                 </div>
-                <div class="z-10 flex flex-row items-center gap-4 p-8 mb-16 bg-white border-2 border-gray rounded-3xl">
+                <!-- Bloc 3 -->
+                <div
+                    class="flex flex-col lg:flex-row items-center gap-4 p-8 mb-16 bg-white border-2 border-gray rounded-3xl">
                     <div class="flex items-center h-full px-4 bg-gray-200 rounded-xl">
-                        <img src="/face-happy.png" alt="smiley icon" class="icons-2 ">
+                        <img src="/face-happy.png" alt="smiley icon" class="icons-2">
                     </div>
-                    <div class="flex flex-col gap-4">
+                    <div class="flex flex-col w-full gap-4">
                         <h3 class="text-xl font-semibold">Profitez de votre trajet</h3>
                         <p>Récupérez votre voiture à l'emplacement désigné et profitez de votre expérience de conduite
                             haut de gamme avec notre service de qualité supérieure.</p>
                     </div>
                 </div>
 
-                <div class="absolute right-0 w-40 h-full bg-gray-100 -z-10 rounded-l-3xl"></div>
+                <div class="absolute right-0 w-40 h-full bg-gray-100 -z-10 rounded-l-3xl hidden lg:block"></div>
             </div>
 
-            <div class="flex items-center p-8 bg-gray-100 rounded-r-full">
-                <img src="/jeep.png" alt="jeep car">
+            <div class="flex items-center p-8 bg-gray-100 rounded-r-full hidden lg:block">
+                <img src="/jeep.png" alt="jeep car" class="w-full h-auto">
             </div>
         </div>
-
     </section>
+
+
+
 
     <section class="px-32 py-32 text-black bg-gray-100">
         <h2 class="mb-16 text-4xl font-bold">Richard Branson a dit ...</h2>
@@ -283,15 +284,15 @@
     </section>
 
     <footer class="text-white bg-black border-t-2 border-gray-200">
-        <div class="flex flex-col lg:flex-row items-center justify-between py-4 lg:px-12">
+        <div class="flex flex-col lg:flex-row items-center justify-between py-4 px-4 lg:px-12 gap-4">
             <div>
-                <h1 class="text-3xl uppercase logo">Bxcars</h1>
+                <h1 class="text-2xl lg:text-3xl uppercase logo">Bxcars</h1>
             </div>
-            <div class="flex justify-between gap-4">
+            <div class="flex justify-between gap-2 lg:gap-4">
                 <a href="{{ url('/about') }}" class="text-gray-300 transition-colors hover:text-white">À propos</a>
                 <a href="/contact" class="text-gray-300 transition-colors hover:text-white">Contact</a>
             </div>
-            <div class="flex flex-row gap-4">
+            <div class="flex flex-row gap-2 lg:gap-4">
                 <a href="https://www.instagram.com/bx_cars_rental/" target="_blank" rel="noopener noreferrer">
                     <div
                         class="flex items-center justify-center w-8 h-8 transition-transform bg-gray-300 rounded-full hover:bg-gray-200 hover:scale-110">
@@ -318,11 +319,10 @@
                 </a>
             </div>
         </div>
-        <div class="w-full text-sm text-center py-2 bg-yellow-500 custom-font">
+        <div class="w-full text-xs lg:text-sm text-center py-2 bg-yellow-500 custom-font">
             <p>MADE IT WITH PASSION ♥ ~ <a href="http://nawfelajari.be" class="text-white hover:text-gray-800"
                     target="_blank" rel="noopener noreferrer">NAWFEL AJARI</a> &#169; 2024</p>
         </div>
-
     </footer>
 
 
@@ -335,7 +335,7 @@
                     alert("L'agence ne possède actuellement que " + {{ $totalCars }} + " véhicules.");
             });
         }
-    });
+
     </script>
 </body>
 
